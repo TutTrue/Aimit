9cc3a511444ca9c3839a52c6f3488804776f71d5
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2025-01-25 21:48:46 +0200

    fix: update CLI update command usage in output message

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ async fn run() -> Result<(), error::error::AimitError> {
     let settings = settings::Settings::new()?;
     let need_update = settings.version_needs_update().await.unwrap();
     if need_update {
-        println!("There is a new available version of Aimit. run `aimit update` to update.\n");
+        println!("There is a new available version of Aimit. run `aimit --update` to update.\n");
     }
     let path = std::path::Path::new(".");
     let diff = git::diff::get_staged_diff(path)?;
