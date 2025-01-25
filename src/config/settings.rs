ff0261524d095ae9378c52ed233f67ff1250ffdc
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2025-01-25 16:44:56 +0200

    feat: enhance settings to create default configuration if not found

--- src/config/settings.rs
+++ src/config/settings.rs
@@ -1,6 +1,6 @@
+use home::home_dir;
 use serde::{Deserialize, Serialize};
 use std::fs;
-use home::home_dir;
 
 use crate::models::ModelType;
 
@@ -23,9 +23,14 @@ impl Settings {
     pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
         let home = home_dir().ok_or("Could not find home directory")?;
         let config_path = home.join(CONFIG_FILE);
-        let settings = fs::read_to_string(config_path)?;
-        let settings: Settings = toml::from_str(&settings)?;
-        Ok(settings)
+
+        if config_path.exists() {
+            let settings = fs::read_to_string(config_path)?;
+            let settings: Settings = toml::from_str(&settings)?;
+            Ok(settings)
+        } else {
+            Self::create_default()
+        }
     }
 
     pub fn get_prompt(&self) -> &str {
@@ -60,5 +65,34 @@ impl Settings {
         match service {
             ModelType::GEMINI => self.api_keys.gemini_api_key.as_deref(),
         }
+    }
+
+    fn create_default() -> Result<Self, Box<dyn std::error::Error>> {
+        let home = home_dir().ok_or("Could not find home directory")?;
+        let config_path = home.join(CONFIG_FILE);
+
+        if let Some(parent) = config_path.parent() {
+            fs::create_dir_all(parent)?;
+        }
+
+        let settings = Settings {
+            default_model: ModelType::GEMINI,
+            prompt: r#"Analyze the following git diff and generate a concise and meaningful commit message summarizing the changes.
+The commit message should follow best practices, including a short title and an optional detailed description if necessary.
+Requirements:
+  Title: 50 characters or less, summarizing the change.
+  Optional Description: If the change requires context, provide a brief explanation in the body.
+
+git diff:
+
+"#.to_string(),
+            api_keys: ApiKeysConfig {
+                gemini_api_key: None,
+                deepseek_api_key: None,
+            },
+        };
+        let settings_str = toml::to_string(&settings)?;
+        fs::write(config_path, settings_str)?;
+        Ok(settings)
     }
 }
